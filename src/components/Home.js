fed61e969a8b47d4b8425e8fff294f063dbcb773
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-30 21:54:09 +0100

    Homepage changes on text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
+document.addEventListener('DomContentLoaded', () => {
+    
+})
 import React from "react";
 
 function Home() {
     return (
-        <div>
-            <p>Welcome to my HomePage</p>
+        <div className = "presentation">
+            <p className ="hi-there">HI THERE! 👋 , I'M</p>
+            <h1 className="name">Aintzane.</h1>
+
+            <h3 className = "profession"><strong>FULL STACK DEVELOPER</strong></h3>
+            <p>🎯 My goal is focused on solving <strong>technological challenges</strong> that help transform <strong>people's life</strong> and <strong>business opportunities</strong> into <strong>realities</strong>. </p>
+            <p>💛 My drivers are <strong>Education</strong> | <strong>Innovation</strong> | <strong>Technology</strong> </p>
+            <button className = "btn-cv">
+                <a className = "btn-link-cv" href = "https://drive.google.com/file/d/1bSM96eNm0aOk2cQ-7WIvcPg7-AZNYlVV/view?usp=sharing" download = "CVAintzane Morales">Check my CV</a>
+            </button>
         </div>
     )
 }
