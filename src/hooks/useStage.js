162945a8a8338417a5852b2a9513e0cb4bc630be
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-12-01 18:53:26 +0100

    funcionality to updateStage with UseEffect Hook

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,33 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { createStage } from '../components/gameHelpers';
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+
+    useEffect(() => {
+        const updateStage = prevStage => {
+            // clear stage
+            const newStage = prevStage.map(row =>
+                row.map(cell=>(cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            )
+            //remove-empty tetromino
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if(value !==0) {
+                        newStage[y + player.position.y][x + player.position.x] = [
+                            value,
+                            `${player.collided ? "merged" : "clear"}`,
+                        ]
+                    }
+                })
+            })
+            return newStage;
+        }
+        setStage(prevState => updateStage(prevState))
+
+        //dependencies
+
+    }, [player])
 
     return [stage, setStage];
 }
