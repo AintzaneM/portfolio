7c24ed7d47a2c36ebc95527a5988b4454465ebc9
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-12-14 14:13:00 +0100

    added links to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,36 +4,49 @@ export const myProjects = [
     {
         id: "1a",
         name: "Who invent what?",
+        about:"",
         img:"",
         year: 2021,
         technologies_tools: "JavaScript, HTML, CSS, DOM Manipulation",
         type: "Game project",
-
+        deployment: "GitHub",
+        web: "https://aintzanem.github.io/Who-invent-what/"
     },
     {
         id: "1b",
-        name: "SurfOn",
-        img:"",
-        year: 2021,
-        technologies_tools: "Express, Moongose, JavaScript, HTML, CSS, Authorization and Authentication",
-        type: "Full Stack application",
-    },
-    {
-        id: "1c",
-        name: "AboutSkills",
-        img:"",
-        year: 2021,
-        technologies_tools: "React, Express, Moongose, REST API, JavaScript, HTML, CSS, Authorization and Authentication",
-        type: "MERN Stack application",
-    },
-    {
-        id: "1d",
         name: "Tetris",
+        about:"",
         img:"",
         year: 2021,
         technologies_tools: "React, JavaScript, HTML, CSS, Styled-Components, React Hooks",
         type: "Computer project",
+        deployment: "Netlify",
+        web: "https://tetris-game-aintzanem.netlify.app/"
+    },
+    {
+        id: "1c",
+        name: "AboutSkills",
+        about:"",
+        img:"",
+        year: 2021,
+        technologies_tools: "React, Express, Moongose, REST API, JavaScript, HTML, CSS, Authorization and Authentication",
+        type: "MERN Stack application",
+        deployment: "Netlify",
+        web: "https://about-skills.netlify.app/"
+    },
+    {
+        id: "1d",
+        name: "SurfOn",
+        about:"",
+        img:"",
+        year: 2021,
+        technologies_tools: "Express, Moongose, JavaScript, HTML, CSS, Authorization and Authentication",
+        type: "Full Stack application",
+        deployment: "Heroku",
+        web:"https://surf-on.herokuapp.com/",
     }
+    
+   
 ]
 
 export const projects = () => {
@@ -42,15 +55,19 @@ export const projects = () => {
             <p>My Projects:</p>
             {myProjects.map((eachProject, index) => {
                 return (
-                    
                     <div className="item-projects" key={eachProject.id}>
                         <p>
                         {/* {console.log(eachProject.id)} */}
                             <Link to={`/projects/${eachProject.id}`}>{eachProject.name}</Link>
                         </p>
-                        <p>
-                            {eachProject.technologies_tools}
-                        </p>
+                        {/* <img>{eachProject.img}</img> */}
+                        <p><strong>About: </strong> <br/>{eachProject.about}</p>
+                        <p><strong>Built with: </strong> <br/>{eachProject.technologies_tools}</p>
+                        <p><strong>Deployment: </strong> <br/>{eachProject.deployment}</p>
+                        <button>
+                            <a href={eachProject.web}>Play!</a>
+                        </button>
+
                     </div> 
                 )
             })}
