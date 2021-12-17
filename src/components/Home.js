a0d372a7af8628c2807ffc1ed4faa86fc658808e
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-12-17 11:31:36 +0100

    button cv

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,22 +6,26 @@ function Home() {
     return (
         <>
         <div className = "presentation">
+            <div className="letter-home">
             <p className ="hi-there">HI THERE! 👋 , I'M</p>
             <h1 className="name">Aintzane.</h1>
             <br/>
 
             <h3 className = "profession"><strong>FULL STACK DEVELOPER</strong></h3>
             <br/>
-            <p>🎯 My goal is focused on solving <strong>technological challenges</strong> that help transform <strong>people's life</strong> and <strong>business opportunities</strong> into <strong>realities</strong>. </p>
             <p>💛 My drivers are <strong>Education</strong> | <strong>Innovation</strong> | <strong>Technology</strong> </p>
+            <p>🎯 My goal is focused on solving <strong>technological challenges</strong> that help transform <strong>people's life</strong> and <strong>business opportunities</strong> into <strong>realities</strong>. </p>
+            </div>
             <br/>
+
+            <div className="container-btn-cv">
+                <button className = "btn-cv">
+                    <a className = "btn-link-cv" href = "https://drive.google.com/file/d/1bSM96eNm0aOk2cQ-7WIvcPg7-AZNYlVV/view?usp=sharing" download = "CVAintzane Morales"><strong>Check my CV</strong></a>
+                </button>
+            </div>
         </div>
 
-        <div className="container-btn-cv">
-            <button className = "btn-cv">
-                <a className = "btn-link-cv" href = "https://drive.google.com/file/d/1bSM96eNm0aOk2cQ-7WIvcPg7-AZNYlVV/view?usp=sharing" download = "CVAintzane Morales"><strong>Check my CV</strong></a>
-            </button>
-        </div>
+       
         <br/>
         <div className = "container-logos">
             <p className="text-work"><strong>I work with:</strong></p>
